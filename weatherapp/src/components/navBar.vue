41aed5d4d96cd79535db157874c62fa206b5fbe2
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-07-31 10:53:47 +0800

    navbar created for weatherApp

---
+++ weatherapp/src/components/navBar.vue
@@ -0,0 +1,57 @@
+<template>
+    <nav class="bg-gray-400 flex items-center text-white p-4 mb-4 justify-between w-full py-8 top-0">
+      <button class="text-xl font-semibold cursor-pointer hover:text-2xl duration-300">
+        <span class="font-semibold text-purple-500 dark:text-purple-400">Forecast</span>27
+      </button>
+      <ul class="hidden md:flex space-x-6 text-lg font-semibold">
+        <li class="hover:text-2xl duration-300"><a href="#home">Home</a></li>
+        <li class="hover:text-2xl duration-300"><a href="#about">News</a></li>
+        <li class="hover:text-2xl duration-300"><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div class="flex items-center space-x-4">
+        <input 
+        type="text" 
+        placeholder="Search..." 
+        class="p-2 rounded text-gray-800 bg-white focus:outline-none focus:ring-2 focus:ring-purple-500" />
+      </div>
+
+      <button @click="toggleMobileMenu" class="md:hidden p-2 rounded-lg text-purple-600 cursor-pointer focus:outline-none focus:ring-1 focus:ring-purple-500 duration-200">
+        <span class="text-xl">{{ isMobileMenuOpen ? '✕' : '☰' }}</span>
+      </button>
+
+      <div v-if="isMobileMenuOpen" class=" absolute top-full left-0 right-0 md:hidden bg-gray-400 text-white shadow-lg border-t border-gray-300">
+        <ul class="flex flex-col space-y-2 p-4">
+          <li><a href="#home" class="block py-2 px-4 hover:bg-purple-500">Home</a></li>
+          <li><a href="#about" class="block py-2 px-4 hover:bg-purple-500">News</a></li>
+          <li><a href="#contact" class="block py-2 px-4 hover:bg-purple-500">Contact</a></li>
+          <li class="py-2">
+          <input 
+            type="text" 
+            placeholder="Search weather..." 
+            class="w-full p-2 rounded text-gray-800 bg-white focus:outline-none focus:ring-2 focus:ring-purple-500" 
+          />
+        </li>
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script lang="js">
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const isMobileMenuOpen = ref(false);
+
+    const toggleMobileMenu = () => {
+      isMobileMenuOpen.value = !isMobileMenuOpen.value;
+    };
+
+    return {
+      isMobileMenuOpen,
+      toggleMobileMenu
+    };
+  }
+};
+</script>
---
+++ weatherapp/src/pages/main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="">
+        <Navbar/>
+    </div>
+  
+</template>
+
+<script setup>
+// import { ref } from 'vue';
+// import { useRoute } from 'vue-router';
+import Navbar from '../components/navBar.vue';
+
+// Your reactive data and logic here
+</script>
